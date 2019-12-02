b756fec70ccefd2de71d91672672b022474609ff
jgerdes96 <58370404+jgerdes96@users.noreply.github.com>
2019-12-02 15:53:25 +0100

    Dateien vom Datenumwandler

---
+++ Main.vb
@@ -0,0 +1,87 @@
+﻿Imports DocumentFormat.OpenXml
+Imports ClosedXML.Excel
+Imports System.IO
+Imports System.Text
+
+Module Datenumwandler
+
+    Sub Main()
+
+        'Abfragen und Öffnen der zu umwandelnden Excel-Datei
+        Console.WriteLine("Geben Sie den Namen der Datei ein, die umgewandelt werden soll")
+        Dim name As String = Console.ReadLine()
+        Dim excelFile As Object = OpenExcel(name)
+
+        'Erstellen der neuen Excel-Datei
+        Dim newWorkbook = New XLWorkbook()
+        Dim newWorksheet As ClosedXML.Excel.IXLWorksheet = newWorkbook.Worksheets.Add("Results" & Date.Now.ToShortDateString)
+
+        'Anlegen einer Liste, in der alle Fragebögen gespeichert werden
+        Dim listOfQuestionnaires As New List(Of Questionnaire)
+        Dim y As Integer = 2
+        Dim cell = excelFile.application.cells(y, 1).value
+
+        'Hier werden jetzt alle Zeilen der Datei, die umgewanldet werden soll, durchgegangen. Jede Zeile enthält den Inhalt eines Fragebogens. 
+        While Not IsNothing(cell)
+
+            'Für jeden Fragebogen wird eine neue Instanz der Klasse Questionnaire angelegt, die alle nötigen Informationen speichert
+            Dim newQuestionnaire As New Questionnaire(y, excelFile)
+
+            'Damit in der neue Excel-Datei nicht mehrmals der identische Schulcode mit identischer laufender Nummer zu finden ist,
+            'wird hier abgefragt, ob der Schulcode mit der laufenden Nummer bereits abgespeichert ist.
+            If Not listOfQuestionnaires.Any(Function(x) x.id = newQuestionnaire.id) And Not newQuestionnaire.id = "" Then
+                listOfQuestionnaires.Add(newQuestionnaire)
+            End If
+
+            y = y + 1
+            cell = excelFile.application.cells(y, 1).value
+
+        End While
+
+        'Hier werden alle Fragebögen sortiert anhand der Anzahl der gestellten Fragen, sodass in der neuen Excel-Datei oben die Fragebögen 
+        'mit den meisten Fragen zu finden sind
+        listOfQuestionnaires = listOfQuestionnaires.OrderByDescending(Function(x) x.qcount).ToList()
+
+        'Hier werden nun die sortierten und überprüften Fragebögen in die neue Excel-Datei geschrieben
+        Dim i As Integer = 1
+        For Each questionnaire In listOfQuestionnaires
+            questionnaire.writoToExcel(newWorksheet, i)
+            i = i + 1
+        Next
+
+        'Schließen der Auslese Datei und speichern und öffnen der neuen Excel Datei
+        SaveAndClose(excelFile, newWorkbook, name)
+
+    End Sub
+
+    Private Function OpenExcel(ByVal name As String) As Object
+
+        'Festlegen des Pfades. Die Datei die umgewandelt werden soll, muss sich um gleichen Ordner befinden, wie diese Anwendung
+        Dim excelPath As String = My.Application.Info.DirectoryPath & "\" & name & ".xlsx"
+
+        'Auslesen der Excel-Datei
+        Dim excelFile As Object = CreateObject("Excel.Application")
+        excelFile.application.workbooks.Open(excelPath)
+        excelFile.application.sheets(1).Select()
+
+        Return excelFile
+
+    End Function
+
+    Private Sub SaveAndClose(ByVal excelFile As Object, ByVal newWorkbook As XLWorkbook, ByVal name As String)
+
+        'Schließen der Auslese Datei
+        excelFile.application.DisplayAlerts = False
+        excelFile.ActiveWorkbook.Close()
+        excelFile.application.DisplayAlerts = True
+        excelFile.application.Quit()
+        excelFile = Nothing
+
+        'Speichern und öffnen der neuen Excel Datei
+        Dim filename As String = My.Application.Info.DirectoryPath & "\" & name & "_umgewandeltsortiert.xlsx"
+        newWorkbook.SaveAs(filename)
+        Process.Start(filename)
+
+    End Sub
+
+End Module
---
+++ Questionnaire.vb
@@ -0,0 +1,79 @@
+﻿Public Class Questionnaire
+
+    Property schoolcode As String
+    Property number As String
+    Property id As String
+
+    Property questions As New List(Of String)
+    Property qcount As Integer = 0
+
+    Property assesments As New List(Of String)
+    Property gender As String
+
+    Sub New(ByVal y As Integer, ByVal excelFile As Object)
+
+        'In der 9. Spalte steht der Schulcode
+        schoolcode = excelFile.application.cells(y, 9).value
+
+        'In der 10. Spalte steht die laufende Nummer
+        number = excelFile.application.cells(y, 10).value
+
+        id = schoolcode & number
+
+        'Von Spalte 11 bis 25 stehen die Fragen
+        For i As Integer = 11 To 25
+            Dim question = excelFile.application.cells(y, i).value
+            questions.Add(question)
+            If Not question = "" Then
+                qcount = qcount + 1
+            End If
+        Next
+
+        'Von Spalte 26 bis 30 stehen die EInschätzungen
+        For i As Integer = 26 To 30
+            assesments.Add(excelFile.application.cells(y, i).value)
+        Next
+
+        'In Spalte 31 steht das Geschlecht
+        gender = excelFile.application.cells(y, 31).value
+
+    End Sub
+
+    Sub writoToExcel(ByVal excelWorksheet As ClosedXML.Excel.IXLWorksheet, ByVal i As Integer)
+
+        'In der 1. Spalte soll die Anzahl der Fragen stehen
+        excelWorksheet.Cell(i, 1).Value = qcount
+
+        'In der 2. Spalte soll der Schulcode stehen und in der 2. Spalte die laufende Nummer
+        excelWorksheet.Cell(i, 2).Value = schoolcode
+        excelWorksheet.Cell(i, 3).Value = number
+
+        'Von Spalte 4 zu 18 stehen die Fragen
+        Dim k As Integer = 0
+        For j As Integer = 4 To 18
+            excelWorksheet.Cell(i, j).Value = questions(k)
+            k = k + 1
+        Next
+
+        'Von Spalte 19 zu 23 stehen die Einschätzungen
+        k = 0
+        For j As Integer = 19 To 23
+            excelWorksheet.Cell(i, j).Value = assesments(k)
+            k = k + 1
+        Next
+
+        'In der 24. Spalte soll das Geschlecht stehen
+        excelWorksheet.Cell(i, 24).Value = gender
+
+    End Sub
+
+
+
+
+End Class
+
+
+
+
+
+
